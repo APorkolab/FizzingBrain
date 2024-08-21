f23544ac30c26f7a2887e7b3095895ce0f611b07
APorkolab <adam.porkolab@gmail.com>
2024-08-21 11:27:10 +0200

    Fix circular dependency issue in models, refactor Sequelize model definitions
    - Refactored `model/user.js` and `model/question.js` to avoid circular dependency by passing Sequelize instance directly.
    - Updated `model/index.js` to handle model initialization and associations correctly.
    - Ensured proper export of models in the `db` object.

--- backend/model/question.js
+++ backend/model/question.js
@@ -1,40 +1,37 @@
-const {
-	DataTypes
-} = require('sequelize');
-const db = require('../model/index');
+module.exports = (sequelize, DataTypes) => {
+	const Question = sequelize.define('question', {
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+			allowNull: false
+		},
+		hungarianQuestion: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		englishQuestion: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		hungarianAnswer: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		englishAnswer: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		descriptionHungarian: {
+			type: DataTypes.STRING,
+		},
+		descriptionEnglish: {
+			type: DataTypes.STRING,
+		}
+	}, {
+		freezeTableName: true,
+		timestamps: false
+	});
 
-const Question = db.sequelize.define('question', {
-	id: {
-		type: DataTypes.INTEGER,
-		autoIncrement: true,
-		primaryKey: true,
-		allowNull: false
-	},
-	hungarianQuestion: {
-		type: DataTypes.STRING,
-		allowNull: false
-	},
-	englishQuestion: {
-		type: DataTypes.STRING,
-		allowNull: false
-	},
-	hungarianAnswer: {
-		type: DataTypes.STRING,
-		allowNull: false
-	},
-	englishAnswer: {
-		type: DataTypes.STRING,
-		allowNull: false
-	},
-	descriptionHungarian: {
-		type: DataTypes.STRING,
-	},
-	descriptionEnglish: {
-		type: DataTypes.STRING,
-	}
-}, {
-	freezeTableName: true,
-	timestamps: false
-});
-
-module.exports = Question;
+	return Question;
+};
--- backend/model/user.js
+++ backend/model/user.js
@@ -1,53 +1,49 @@
-const {
-	DataTypes
-} = require('sequelize');
-const bcrypt = require('bcrypt');
-const db = require('../model/index');
-
-const User = db.sequelize.define('User', {
-	id: {
-		autoIncrement: true,
-		primaryKey: true,
-		type: DataTypes.INTEGER
-	},
-	email: {
-		type: DataTypes.STRING,
-		allowNull: false,
-		unique: true
-	},
-	password: {
-		type: DataTypes.STRING,
-		allowNull: false
-	},
-	firstName: {
-		type: DataTypes.STRING,
-		allowNull: false
-	},
-	lastName: {
-		type: DataTypes.STRING,
-		allowNull: false
-	}
-}, {
-	hooks: {
-		beforeCreate: async (user) => {
-			if (user.password) {
-				const salt = await bcrypt.genSalt(10);
-				user.password = await bcrypt.hash(user.password, salt);
+module.exports = (sequelize, DataTypes) => {
+	const User = sequelize.define('User', {
+		id: {
+			autoIncrement: true,
+			primaryKey: true,
+			type: DataTypes.INTEGER
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		firstName: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		lastName: {
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	}, {
+		hooks: {
+			beforeCreate: async (user) => {
+				if (user.password) {
+					const salt = await bcrypt.genSalt(10);
+					user.password = await bcrypt.hash(user.password, salt);
+				}
+			},
+			beforeUpdate: async (user) => {
+				if (user.password && user.changed('password')) {
+					const salt = await bcrypt.genSalt(10);
+					user.password = await bcrypt.hash(user.password, salt);
+				}
 			}
 		},
-		beforeUpdate: async (user) => {
-			if (user.password && user.changed('password')) {
-				const salt = await bcrypt.genSalt(10);
-				user.password = await bcrypt.hash(user.password, salt);
-			}
-		}
-	},
-	freezeTableName: true,
-	timestamps: false
-});
+		freezeTableName: true,
+		timestamps: false
+	});
 
-User.prototype.validPassword = async function (password) {
-	return await bcrypt.compare(password, this.password);
-}
+	User.prototype.validPassword = async function (password) {
+		return await bcrypt.compare(password, this.password);
+	}
 
-module.exports = User;
+	return User;
+};
