04ce2a4e0ef52744bbd8f56fc167fe4480431921
APorkolab <adam.porkolab@gmail.com>
2024-08-21 09:39:57 +0200

    Fix logger not defined issue.

--- backend/wait-for-db.js
+++ backend/wait-for-db.js
@@ -1,3 +1,14 @@
+const logger = require('winston');
+const mysql = require('mysql2/promise');
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+};
+
 async function checkDatabaseConnection() {
   let retries = 5;
   while (retries) {
