d559929829520fee5cd02f36cb893211f5161c03
APorkolab <adam.porkolab@gmail.com>
2024-08-21 07:50:20 +0200

    SQL script mod.

--- backend/SQL database scripts/fkbpanik_fizzingbrain_onlyStructure.sql
+++ backend/SQL database scripts/fkbpanik_fizzingbrain_onlyStructure.sql
@@ -1,35 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Gép: 127.0.0.1
--- Létrehozás ideje: 2022. Nov 12. 15:58
--- Kiszolgáló verziója: 10.4.25-MariaDB
--- PHP verzió: 8.1.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Adatbázis: `fkbpanik_fizzingbrain`
---
 CREATE DATABASE IF NOT EXISTS `fkbpanik_fizzingbrain` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `fkbpanik_fizzingbrain`;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `question`
---
-
-CREATE TABLE `question` (
+CREATE TABLE IF NOT EXISTS `question` (
   `id` int(11) NOT NULL,
   `hungarianQuestion` varchar(2000) NOT NULL,
   `englishQuestion` varchar(2000) NOT NULL,
@@ -37,55 +8,28 @@ CREATE TABLE `question` (
   `englishAnswer` varchar(2000) NOT NULL,
   `descriptionHungarian` varchar(2000) DEFAULT NULL,
   `descriptionEnglish` varchar(2000) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `user`
---
-
-CREATE TABLE `user` (
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL,
   `firstName` varchar(1000) NOT NULL,
   `lastName` varchar(1000) NOT NULL,
   `email` varchar(1000) NOT NULL,
   `password` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexek a kiírt táblákhoz
---
-
---
--- A tábla indexei `question`
---
-ALTER TABLE `question`
-  ADD PRIMARY KEY (`id`);
-
---
--- A tábla indexei `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- A kiírt táblák AUTO_INCREMENT értéke
---
-
---
--- AUTO_INCREMENT a táblához `question`
---
-ALTER TABLE `question`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT a táblához `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+ALTER TABLE
+  `question`
+ADD
+  PRIMARY KEY (`id`);
+ALTER TABLE
+  `user`
+ADD
+  PRIMARY KEY (`id`);
+ALTER TABLE
+  `question`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 1;
+ALTER TABLE
+  `user`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 1;
